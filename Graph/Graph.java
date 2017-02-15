ad63f2ffb38895cd001968bc0699ac2f14dcfc8e
Benjamin15 <benjamin150594@hotmail.fr>
2017-02-15 16:16:23 -0500

    ajout Commentaire
    Ajout des commentaires au fonction vide

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -17,18 +17,41 @@ public class Graph {
 	public void setListArc(ArrayList<Sommet> listSommet) {
 		this.listSommet = listSommet;
 	}
+	
+	/**
+	 * Permet de remplir les objets afin de remplir le graph. Rempli l'arrayList avec tous les arcs.
+	 */
 	public void creerGraph()
 	{
 		
 	}
+	
+	/**
+	 * permet de lire les graph a l'écran
+	 */
+	
 	public void lireGraph()
 	{
 		
 	}
+	
+	/**
+	 * Permet de voir le plus court chemin. Affiche des données en sortie
+	 * 
+	 * @param debut
+	 * @param gainVoulu
+	 */
 	public void plusCourtChemin(Arc debut, int gainVoulu)
 	{
 		
 	}
+	
+	/**
+	 * Permet de voir le plus grand gain à une distance max en partant d'un arc. Doit afficher les valeurs en sorties.
+	 * 
+	 * @param debut
+	 * @param distanceMax
+	 */
 	public void plusGrandGain(Arc debut, int distanceMax)
 	{
 		
